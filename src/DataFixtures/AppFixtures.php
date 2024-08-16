3c91121005f0f415ee3baa3f10db67b9497e84b7
Chris Speare <cspeare81@googlemail.com>
2024-08-16 12:08:37 +0000

    Lesson 71 - N+1 Query

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
 use App\Entity\MicroPost;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -15,14 +16,22 @@ class AppFixtures extends Fixture
 
         $faker = Factory::create();
 
-        for ($i = 0; $i < 1000; $i++) {
+        for ($i = 0; $i < 200; $i++) {
 
             $microPost = new MicroPost();
             $microPost->setTitle($faker->words(3, true));
             $microPost->setText($faker->text);
             $microPost->setCreated($faker->dateTime);
-
             $manager->persist($microPost);
+
+            for ($y = 0; $y < rand(0, 20); $y++) {
+
+                $comment = new Comment();
+                $comment->setText($faker->text);
+                $comment->setPost($microPost);
+                $manager->persist($comment);
+
+            }
         }
 
         $manager->flush();
