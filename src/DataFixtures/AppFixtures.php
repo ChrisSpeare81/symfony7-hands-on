1542d6ee8596db242049f21af3caa783be45aeed
Chris Speare <cspeare81@googlemail.com>
2024-08-16 12:35:43 +0000

    Lesson 74 - User Fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,19 +4,36 @@ namespace App\DataFixtures;
 
 use App\Entity\Comment;
 use App\Entity\MicroPost;
-use DateTime;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {
+
+    }
+
     public function load(ObjectManager $manager): void
     {
 
         $faker = Factory::create();
 
-        for ($i = 0; $i < 200; $i++) {
+        for ($i = 0; $i < rand(10, 50); $i++) {
+
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+            $manager->persist($user);
+
+        }
+
+        for ($i = 0; $i < rand(1, 500); $i++) {
 
             $microPost = new MicroPost();
             $microPost->setTitle($faker->words(3, true));
